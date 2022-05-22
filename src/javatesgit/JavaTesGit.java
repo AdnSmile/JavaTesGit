55fa69ab44969273bfa9d7110a40a517de518783
Gozen <puss8989@gmail.com>
2022-05-22 23:34:20 +0700

    gojen hadir disini

--- src/javatesgit/JavaTesGit.java
+++ src/javatesgit/JavaTesGit.java
@@ -13,6 +13,7 @@ public class JavaTesGit {
      */
     public static void main(String[] args) {
         System.out.println("tolong tulis code apa aja yaa trus comit jangan lupa kasih komen kalian");
+        System.out.println("Vano join the game");
     }
     
 }
