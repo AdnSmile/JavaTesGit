b4e6195778bab28d03efc99946fef8cbd1d9db48
AdnSmile <nessafitri16@gmail.com>
2022-05-22 23:15:09 +0700

    membuat file java baru

---
+++ src/javatesgit/JavaTesGit.java
@@ -0,0 +1,18 @@
+// tes git nih adik-adik
+
+package javatesgit;
+
+/**
+ *
+ * @author nessa
+ */
+public class JavaTesGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("tolong tulis code apa aja yaa trus comit jangan lupa kasih komen kalian");
+    }
+    
+}
